01f2917239ebfe8b8eeb85ddfd7bf34184920400
Kevin Lim <klim0071@student.monash.edu>
2025-09-23 18:29:54 +0800

    Changed HW9_map to use github url

---
+++ vega_lite_HW9.js
@@ -0,0 +1,6 @@
+var vg_1 = "HW9_map.vg.json";
+
+vegaEmbed("#cloropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance 
+    // (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
